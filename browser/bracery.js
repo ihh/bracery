0974a2e2ec58d394f915cc9127fdbfd864f1f24c
Ian Holmes <ihh@berkeley.edu>
2018-05-12 16:37:44 -0700

    published 1.0.37

--- browser/bracery.js
+++ browser/bracery.js
@@ -1,4 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+var Template = require('./template')
 var ParseTree = require('./parsetree')
 var RhsParser = ParseTree.RhsParser
 var extend = ParseTree.extend
@@ -168,10 +169,11 @@ Bracery.prototype.normalize = function (text) {
 module.exports = { Bracery: Bracery,
                    ParseTree: ParseTree,
                    RhsParser: RhsParser,
+                   Template: Template,
                    Promise: Promise,
                    nlp: nlp }
 
-},{"./parsetree":2}],2:[function(require,module,exports){
+},{"./parsetree":2,"./template":5}],2:[function(require,module,exports){
 var RhsParser = require('./rhs')
 
 // General helper functions
@@ -3408,4 +3410,155 @@ module.exports = {
 },{}],4:[function(require,module,exports){
 window.bracery = require('./bracery');
 
-},{"./bracery":1}]},{},[4]);
+},{"./bracery":1}],5:[function(require,module,exports){
+var ParseTree = require('./parsetree')
+var extend = ParseTree.extend
+
+var defaultMaxReplies = 100
+
+function makeTagArray (text) {
+  return text.replace (/^\s*(.*?)\s*$/, function (_m, g) { return g })
+    .split(/\s+/)
+    .map (function (tag) { return tag.toLowerCase() })
+}
+
+function makeTagString (text) {
+  return (text
+          ? (' ' + makeTagArray(text).join(' ') + ' ')
+	  : '')
+}
+
+function parseTemplateDefs (text) {
+  var templates = []
+  try {
+    var newTemplateDefReg = /^(\d*)(@.*|)(>+)\s*(.*?)\s*(#\s*(.*?)\s*(#\s*(.*?)\s*|)|)$/;
+    var replyChain = [], currentTemplate, newTemplateDefMatch
+    text.split(/\n/).forEach (function (line) {
+      if (line.length) {
+        if (currentTemplate)
+          currentTemplate.content = currentTemplate.content.concat (ParseTree.parseRhs (line + '\n'))
+        else if (newTemplateDefMatch = newTemplateDefReg.exec (line)) {
+          var weight = newTemplateDefMatch[1],
+              author = newTemplateDefMatch[2],
+              depth = newTemplateDefMatch[3].length - 1,
+	      title = newTemplateDefMatch[4],
+	      prevTags = makeTagString (newTemplateDefMatch[6]),
+	      tags = makeTagString (newTemplateDefMatch[8])
+          var isRoot = !prevTags.match(/\S/) || (prevTags.search(' root ') >= 0)
+          author = author ? author.substr(1) : null
+          currentTemplate = { title: title,
+                              author: author,
+			      previousTags: prevTags,
+			      tags: tags,
+                              isRoot: isRoot,
+                              weight: weight.length ? parseInt(weight) : undefined,
+			      content: [],
+                              replies: [] }
+          if (depth > replyChain.length)
+            throw new Error ("Missing replies in chain")
+          replyChain = replyChain.slice (0, depth)
+          if (depth > 0)
+            replyChain[depth-1].replies.push (currentTemplate)
+          else
+            templates.push (currentTemplate)
+          replyChain.push (currentTemplate)
+        }
+      } else {
+        // line is empty
+        currentTemplate = undefined
+      }
+    })
+  } catch(e) { console.log(e) }
+  return templates
+}
+
+function sampleTemplate (templates) {
+  var totalWeight = templates.reduce (function (total, template) { return total + (template.weight || 1) }, 0)
+  var w = totalWeight * Math.random()
+  for (var i = 0; i < templates.length; ++i)
+    if ((w -= (templates[i].weight || 1)) <= 0)
+      return templates[i]
+  return undefined
+}
+
+function randomRootTemplate (templates) {
+  return sampleTemplate (templates.filter (function (template) { return template.isRoot }))
+}
+
+function randomReplyTemplate (templates, tags, prevTemplate) {
+  tags = typeof(tags) === 'string' ? makeTagArray(tags) : tags
+  return sampleTemplate (templates.filter (function (template) {
+    if (prevTemplate && prevTemplate.replies.indexOf (template) >= 0)
+      return true
+    var prevTags = template.previousTags.toLowerCase()
+    return tags.reduce (function (match, tag) {
+      return match || (prevTags.search (' ' + tag + ' ') >= 0)
+    }, false)
+  }))
+}
+
+function promiseMessageList (config) {
+  var bracery = config.bracery, templates = config.templates
+  var maxReplies = typeof(config.maxReplies) === 'undefined' ? defaultMaxReplies : config.maxReplies
+  var accept = config.accept || function (expansion, thread) { return true }
+  var prevMessage = config.previousMessage
+  var generateTemplate = (prevMessage
+                          ? randomReplyTemplate.bind (null, templates, prevMessage.tags, prevMessage.template)
+                          : randomRootTemplate.bind (null, templates))
+  function generateMessage() {
+    var message
+    var template = generateTemplate()
+    if (template) {
+      var vars = extend ({}, config.vars || {})
+      message = { template: template,
+                  vars: extend ({}, vars),
+                  expansion: bracery._expandRhs (extend ({},
+                                                         config,
+                                                         { rhs: ParseTree.sampleParseTree (template.content, bracery.rng),
+                                                           vars: vars })) }
+      message.title = vars.title || template.title
+      message.tags = vars.prevtags = vars.tags || template.tags
+      delete vars.tags
+      message.nextVars = extend ({}, vars)
+    }
+    return message
+  }
+  function promiseMessage() {
+    var proposedMessage = generateMessage()
+//    console.warn('proposedMessage',proposedMessage)
+    return new Promise (function (resolve, reject) {
+      if (!proposedMessage)
+        resolve (true)
+      else
+        resolve (accept (proposedMessage, config.thread))
+    }).then (function (accepted) {
+      return accepted ? proposedMessage : promiseMessage()
+    })
+  }
+  return promiseMessage()
+    .then (function (message) {
+      return (message
+              ? ((maxReplies > 0 || typeof(maxReplies) === 'undefined' || maxReplies === null)
+                 ? (promiseMessageList (extend ({},
+                                                config,
+                                                { previousMessage: message,
+                                                  vars: message.nextVars,
+                                                  thread: (config.thread || []).concat (message),
+                                                  maxReplies: (maxReplies ? maxReplies - 1 : maxReplies) }))
+                    .then (function (replies) {
+                      return [message].concat (replies)
+                    }))
+                 : [message])
+              : [])
+    })
+}
+
+module.exports = { parseTemplateDefs: parseTemplateDefs,
+                   sampleTemplate: sampleTemplate,
+                   randomRootTemplate: randomRootTemplate,
+                   randomReplyTemplate: randomReplyTemplate,
+                   promiseMessageList: promiseMessageList,
+                   makeTagArray: makeTagArray,
+                   makeTagString: makeTagString }
+
+},{"./parsetree":2}]},{},[4]);

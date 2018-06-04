1b8d873124a32c1f911ea4ac9ca6986232c5c3f1
Ian Holmes <ihholmes@gmail.com>
2018-06-04 14:26:12 -0700

    toposort

--- src/chomsky.js
+++ src/chomsky.js
@@ -7,7 +7,14 @@ function makeChomskyNormalCFG (bracery, vars, rhs) {
   }
   var cfg = {}
   var start = makeChomskyNormalSymbol (bracery, vars, cfg, [typeof(rhs) === 'string' ? ParseTree.parseRhs(rhs) : rhs])
-  return start ? { cfg: cfg, start: start.name } : null
+  if (!start)
+    return null
+  var toposort = toposortSymbols (cfg)
+  return { cfg: cfg,
+	   empties: toposort.empties,
+	   cyclic: toposort.cyclic,
+	   sort: toposort.sort,
+	   start: start.name }
 }
 
 function makeChomskyNormalRules (bracery, vars, cfg, name) {
@@ -57,6 +64,112 @@ function makeChomskyNormalSymbol (bracery, vars, cfg, rhsList, name, weight) {
     })
   }
   return cfg[name] ? { type: 'nonterm', name: name } : null
+}
+
+function getSymbols (cfg) {
+  return Object.keys (cfg)
+}
+
+function getSources (cfg) {
+  var isSource = {}, symbols = getSymbols (cfg)
+  symbols.forEach (function (sym) {
+    cfg[sym].forEach (function (rhs) {
+      rhs.rhs.forEach (function (node) {
+	if (node.type === 'nonterm') {
+	  isSource[node.name] = isSource[node.name] || {}
+	  isSource[node.name][sym] = true
+	}
+      })
+    })
+  })
+  var sources = {}
+  symbols.forEach (function (sym) {
+    sources[sym] = isSource[sym] ? Object.keys(isSource[sym]).sort() : []
+  })
+  return sources
+}
+
+function nodeIsNonempty (cfg, flaggedAsEmpty, node) {
+  return (node.type === 'term' ? (node.text.length > 0) : (cfg[node.name] && !flaggedAsEmpty[node.name]))
+}
+
+function symIsEmpty (cfg, flaggedAsEmpty, sym) {
+  return cfg[sym].reduce (function (foundEmptyRhs, rhs) {
+    return foundEmptyRhs || !rhs.rhs.filter (nodeIsNonempty.bind (null, cfg, flaggedAsEmpty)).length
+  }, false)
+}
+
+function getEmptyFlags (cfg) {
+  var sources = getSources (cfg), symbols = getSymbols (cfg)
+  var flaggedAsEmpty = {}
+  do {
+    var foundEmpties = false
+    symbols.forEach (function (sym) {
+      if (!flaggedAsEmpty[sym] && symIsEmpty(cfg,flaggedAsEmpty,sym)) {
+	flaggedAsEmpty[sym] = true
+	foundEmpties = true
+      }
+    })
+  } while (foundEmpties)
+  return flaggedAsEmpty
+}
+
+function getNullTransitions (cfg) {
+  var isEmpty = getEmptyFlags (cfg), symbols = getSymbols (cfg)
+  var isSource = {}, isSink = {}
+  symbols.forEach (function (sym) {
+    isSource[sym] = {}
+    isSink[sym] = {}
+  })
+  symbols.forEach (function (source) {
+    cfg[source].forEach (function (rhs) {
+      rhs.rhs.forEach (function (node) {
+	if (node.type === 'nonterm' && !rhs.rhs.filter (function (otherNode) {
+	  return otherNode !== node && nodeIsNonempty (cfg, isEmpty, otherNode)
+	}).length) {
+	var sink = node.name
+	  isSink[source][sink] = true
+	  isSource[sink][source] = true
+	}
+      })
+    })
+  })
+  var sinks = {}, sources = {}
+  symbols.forEach (function (sym) {
+    sinks[sym] = Object.keys (isSink[sym]).sort()
+    sources[sym] = Object.keys (isSource[sym]).sort()
+  })
+  return { empties: Object.keys(isEmpty).sort(), sinks: sinks, sources: sources }
+}
+
+function toposortSymbols (cfg) {
+  var symbols = getSymbols (cfg), trans = getNullTransitions (cfg)
+  // Kahn, Arthur B. (1962), "Topological sorting of large networks", Communications of the ACM 5 (11): 558–562, doi:10.1145/368996.369025
+  // https://en.wikipedia.org/wiki/Topological_sorting
+  var S = [], L = []
+  var nParents = [], edges = 0
+  symbols.forEach (function (c) {
+    nParents[c] = trans.sources[c].length
+    edges += nParents[c]
+    if (nParents[c] == 0)
+      S.push (c)
+  })
+  while (S.length > 0) {
+    var n = S.shift()
+    L.push (n)
+    trans.sinks[n].forEach (function(m) {
+      --edges
+      if (--nParents[m] == 0)
+        S.push (m)
+    })
+  }
+
+  if (edges > 0)
+    trans.cyclic = true
+  else
+    trans.sort = L
+
+  return trans
 }
 
 module.exports = { makeChomskyNormalCFG: makeChomskyNormalCFG }
--- test/chomsky.js
+++ test/chomsky.js
@@ -4,7 +4,13 @@ var bracery = require('../index')
 var initJson = { hello: "[yo|hi]",
 		 world: ["world", "planet", "kids", "#hello#xxx#hello##hello#zzz"] }
 
-var chomskyJson = {cfg:{"1":[{rhs:[{type:"nonterm",name:"hello"},{type:"nonterm",name:"6"}],weight:1}],"2":[{rhs:[{type:"term",text:"yo"}],weight:0.5},{rhs:[{type:"term",text:"hi"}],weight:0.5}],"3":[{rhs:[{type:"nonterm",name:"hello"},{type:"term",text:"zzz"}],weight:1}],"4":[{rhs:[{type:"nonterm",name:"hello"},{type:"nonterm",name:"3"}],weight:1}],"5":[{rhs:[{type:"term",text:"xxx"},{type:"nonterm",name:"4"}],weight:1}],"6":[{rhs:[{type:"term",text:" "},{type:"nonterm",name:"world"}],weight:1}],world:[{rhs:[{type:"term",text:"world"}],weight:0.25},{rhs:[{type:"term",text:"planet"}],weight:0.25},{rhs:[{type:"term",text:"kids"}],weight:0.25},{rhs:[{type:"nonterm",name:"hello"},{type:"nonterm",name:"5"}],weight:0.25}],hello:[{rhs:[{type:"nonterm",name:"2"}],weight:1}]},start:"1"}
+var chomskyJson = {"cfg":{"1":[{"rhs":[{"type":"nonterm","name":"hello"},{"type":"nonterm","name":"6"}],"weight":1}],"2":[{"rhs":[{"type":"term","text":"yo"}],"weight":0.5},{"rhs":[{"type":"term","text":"hi"}],"weight":0.5}],"3":[{"rhs":[{"type":"nonterm","name":"hello"},{"type":"term","text":"zzz"}],"weight":1}],"4":[{"rhs":[{"type":"nonterm","name":"hello"},{"type":"nonterm","name":"3"}],"weight":1}],"5":[{"rhs":[{"type":"term","text":"xxx"},{"type":"nonterm","name":"4"}],"weight":1}],"6":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"world"}],"weight":1}],"world":[{"rhs":[{"type":"term","text":"world"}],"weight":0.25},{"rhs":[{"type":"term","text":"planet"}],"weight":0.25},{"rhs":[{"type":"term","text":"kids"}],"weight":0.25},{"rhs":[{"type":"nonterm","name":"hello"},{"type":"nonterm","name":"5"}],"weight":0.25}],"hello":[{"rhs":[{"type":"nonterm","name":"2"}],"weight":1}]},"empties":[],"sort":["1","3","4","5","6","world","hello","2"],"start":"1"}
+
+var cyclicJson = {a:["~b ~c","~b~d"],b:["~e~f"],c:["~f ~g"],d:"",e:["","~a"],f:["[|x]"],g:"x"}
+var chomskyCyclicJson = {"cfg":{"1":[{"rhs":[{"type":"nonterm","name":"a"}],"weight":1}],"2":[{"rhs":[],"weight":0.5},{"rhs":[{"type":"term","text":"x"}],"weight":0.5}],"3":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"g"}],"weight":1}],"4":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"c"}],"weight":1}],"a":[{"rhs":[{"type":"nonterm","name":"b"},{"type":"nonterm","name":"4"}],"weight":0.5},{"rhs":[{"type":"nonterm","name":"b"},{"type":"nonterm","name":"d"}],"weight":0.5}],"c":[{"rhs":[{"type":"nonterm","name":"f"},{"type":"nonterm","name":"3"}],"weight":1}],"g":[{"rhs":[{"type":"term","text":"x"}],"weight":1}],"f":[{"rhs":[{"type":"nonterm","name":"2"}],"weight":1}],"b":[{"rhs":[{"type":"nonterm","name":"e"},{"type":"nonterm","name":"f"}],"weight":1}],"e":[{"rhs":[],"weight":0.5},{"rhs":[{"type":"nonterm","name":"a"}],"weight":0.5}],"d":[{"rhs":[],"weight":1}]},"empties":["1","2","a","b","d","e","f"],"cyclic":true,"start":"1"}
+
+var topoJson = {a:["~b ~c","~b ~d"],b:["~e ~f"],c:["~f ~g"],d:"",e:["","~a"],f:["[|x]"],g:"x"}
+var chomskyTopoJson = {"cfg":{"1":[{"rhs":[{"type":"nonterm","name":"a"}],"weight":1}],"2":[{"rhs":[],"weight":0.5},{"rhs":[{"type":"term","text":"x"}],"weight":0.5}],"3":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"g"}],"weight":1}],"4":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"f"}],"weight":1}],"5":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"c"}],"weight":1}],"6":[{"rhs":[{"type":"term","text":" "},{"type":"nonterm","name":"d"}],"weight":1}],"a":[{"rhs":[{"type":"nonterm","name":"b"},{"type":"nonterm","name":"5"}],"weight":0.5},{"rhs":[{"type":"nonterm","name":"b"},{"type":"nonterm","name":"6"}],"weight":0.5}],"c":[{"rhs":[{"type":"nonterm","name":"f"},{"type":"nonterm","name":"3"}],"weight":1}],"g":[{"rhs":[{"type":"term","text":"x"}],"weight":1}],"f":[{"rhs":[{"type":"nonterm","name":"2"}],"weight":1}],"b":[{"rhs":[{"type":"nonterm","name":"e"},{"type":"nonterm","name":"4"}],"weight":1}],"e":[{"rhs":[],"weight":0.5},{"rhs":[{"type":"nonterm","name":"a"}],"weight":0.5}],"d":[{"rhs":[],"weight":1}]},"empties":["2","d","e","f"],"sort":["1","5","6","c","g","f","b","e","d","3","2","4","a"],"start":"1"}
 
 var b = new bracery.Bracery (initJson)
 describe('Chomsky normal form', function() {
@@ -13,4 +19,19 @@ describe('Chomsky normal form', function() {
     assert.equal (JSON.stringify(json), JSON.stringify(chomskyJson))
     done()
   })
+
+  it('should find a null cycle', function (done) {
+    var b2 = new bracery.Bracery (cyclicJson)
+    var json2 = bracery.Chomsky.makeChomskyNormalCFG (b2, '~a')
+    assert.equal (JSON.stringify(json2), JSON.stringify(chomskyCyclicJson))
+    done()
+  })
+
+  it('should toposort', function (done) {
+    var b3 = new bracery.Bracery (topoJson)
+    var json3 = bracery.Chomsky.makeChomskyNormalCFG (b3, '~a')
+    assert.equal (JSON.stringify(json3), JSON.stringify(chomskyTopoJson))
+    done()
+  })
+
 })

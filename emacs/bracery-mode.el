a17da45d4c3d1dbd6ede69de902dfab7338c053f
Ian Holmes <ihh@berkeley.edu>
2018-12-09 16:41:07 -0800

    emacs mode

---
+++ emacs/bracery-mode.el
@@ -0,0 +1,46 @@
+;; https://www.emacswiki.org/emacs/ModeTutorial
+;; http://ergoemacs.org/emacs/elisp_syntax_coloring.html
+
+(defvar bracery-mode-hook nil)
+
+(defvar bracery-mode-map
+  (let ((map (make-keymap)))
+    (define-key map "\C-j" 'newline-and-indent)
+    map)
+  "Keymap for bracery major mode")
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.bracery\\'" . bracery-mode))
+
+(setq bracery-field-regexp "#[a-z0-9]+ +\\(.*\\)")
+
+(setq bracery-highlights
+      '(("^#[^\n]*" . font-lock-comment-face)  ;; red (comments)
+        ("#[A-Za-z0-9_]#" . font-lock-function-name-face)  ;; blue (#xxx#)
+	("^>[^#]+[^ #\n]" . font-lock-warning-face)  ;; magenta (start of header line)
+        (" #.*" . font-lock-variable-name-face)  ;; yellow (tags on header line)
+        ("~[A-Za-z0-9_]+" . font-lock-function-name-face)  ;; blue (~xxx)
+        ("$[A-Za-z0-9_]+" . font-lock-constant-face)  ;; 
+        ("&[A-Za-z0-9_]+" . font-lock-keyword-face)  ;; cyan
+        ))
+
+(defvar bracery-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    ;; The following line ought to prevent '"' being auto-highlighted as a quote, but doesn't seem to work...
+    (modify-syntax-entry ?\" "." st)
+    (modify-syntax-entry ?_ "w" st)
+    st)
+  "Syntax table for bracery-mode")
+
+(defun bracery-mode ()
+  "Major mode for editing bracery files"
+  (interactive)
+  (kill-all-local-variables)
+  (set-syntax-table bracery-mode-syntax-table)
+  (use-local-map bracery-mode-map)
+  (setq font-lock-defaults '(bracery-highlights))
+  (set-syntax-table bracery-mode-syntax-table)
+  (setq mode-name "bracery")
+  (run-hooks 'bracery-mode-hook))
+
+(provide 'bracery-mode)

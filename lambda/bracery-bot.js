d9499d6ca6363814d8a9feab0a5b9ed04b408ceb
Ian Holmes <ihh@berkeley.edu>
2019-03-12 10:31:38 -0700

    bracery-bot.

--- lambda/bracery-bot.js
+++ lambda/bracery-bot.js
@@ -11,7 +11,8 @@ const Twit = require('twit');
 const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;  // must be defined from AWS Lambda
 const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;  // must be defined from AWS Lambda
 
-const millisecsBetweenTweets = 15*60*1000 / 300;   // Twitter allows 300 tweets per 15-minute window
+const maxTweetsPerCall = 300;   // Twitter allows 300 tweets per 15-minute window
+const callsPerDay = 4 * 24;  // On average we want each account to tweet once per day
 
 const dynamoPromise = util.dynamoPromise();
 
@@ -42,20 +43,26 @@ exports.handler = async (event, context, callback) => {
       byScreenName[sn].push (item);
     });
     const screenNames = Object.keys (byScreenName);
-    for (let i = 0; i < screenNames.length; ++i) {
-      const items = byScreenName[screenNames[i]];
+    for (let i = 0, tweets = 0; i < screenNames.length && tweets < maxTweetsPerCall; ++i) {
+      // Fisher-Yates shuffle
+      const j = i + Math.floor (Math.random() * (screenNames.length - i));
+      const items = byScreenName[screenNames[j]];
+      screenNames[j] = screenNames[i];
+      // This function is called many times per day, so tweet with a probability that (on average) yields one tweet per day
+      if (Math.random() >= 1 / callsPerDay)
+        continue;
+      // Pick a random bracery word to expand
       const item = items[Math.floor (Math.random() * items.length)];
-      let rateLimitPromise = util.promiseDelay (millisecsBetweenTweets);
+      vars = item.vars ? JSON.parse (item.vars) : {};
+      let expansion = await braceryConfig.expandFull ({ symbolName: item.name });
+      let html = util.expandMarkdown (expansion.text);
+      let digest = util.digestHTML (html);
       let twit = new Twit({
         consumer_key: TWITTER_CONSUMER_KEY,
         consumer_secret: TWITTER_CONSUMER_SECRET,
         access_token: item.accessToken,
         access_token_secret: item.accessTokenSecret
       });
-      vars = item.vars ? JSON.parse (item.vars) : {};
-      let expansion = await braceryConfig.expandFull ({ symbolName: item.name });
-      let html = util.expandMarkdown (expansion.text);
-      let digest = util.digestHTML (html);
       console.warn('Tweeting as @' + item.twitterScreenName + ': ' + digest);
       await twit.post('statuses/update', { status: expansion.text });
       await dynamoPromise('update')
@@ -66,7 +73,7 @@ exports.handler = async (event, context, callback) => {
          ExpressionAttributeNames: { '#v': 'vars' },
          ExpressionAttributeValues: { ':v': JSON.stringify (expansion.vars) },
        });
-      await rateLimitPromise;
+      ++tweets;
     }
   }
 

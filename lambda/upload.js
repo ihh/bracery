6294203084a90b22bff27b8047a5384d20b63dcc
Ian Holmes <ihh@berkeley.edu>
2019-03-11 23:03:53 -0700

    uploads

--- lambda/upload.js
+++ lambda/upload.js
@@ -10,16 +10,26 @@ var util = require('./bracery-util')
 // parse command-line options
 var opt = getopt.create([
   ['d' , 'data=PATH+'       , 'upload data file(s) with format [{"name":"symbol1","rules":["option1","option2"...]},{"name":"symbol2","rules":[...]},...]'],
+  ['s' , 'symbols=NAME+'    , 'include only named symbols'],
+  ['b' , 'begin=NAME'       , 'begin at named symbol'],
   ['h' , 'help'             , 'display this help message']
 ])              // create Getopt instance
     .bindHelp()     // bind option 'help' to default action
     .parseSystem() // parse command line
 
 try {
-  var dataFiles = (opt.options.data || []).concat (opt.argv)
+  const dataFiles = (opt.options.data || []).concat (opt.argv);
   if (!(dataFiles && dataFiles.length))
     throw new Error ('Please specify a data file');
 
+  let symbolAllowed = null, firstSymbol = null, begun = false;
+  if (opt.options.symbols) {
+    symbolAllowed = {};
+    opt.options.symbols.forEach ((sym) => (symbolAllowed[sym] = true));
+  }
+  if (opt.options.begin)
+    firstSymbol = opt.options.begin;
+  
   dataFiles.reduce (async (previousFiles, filename) => {
 
     await previousFiles;
@@ -29,6 +39,17 @@ try {
 
     await defs.reduce (async (previousDefs, def) => {
       await previousDefs;
+      if (symbolAllowed && !symbolAllowed[def.name]) {
+        console.warn (def.name + ' not whitelisted');
+        return;
+      } if (firstSymbol) {
+        if (def.name === firstSymbol)
+          begun = true;
+        if (!begun) {
+          console.warn (def.name + ' skipped');
+          return;
+        }
+      }
       const opts = {
         hostname: 'bracery.org',
         port: 443,
@@ -42,7 +63,7 @@ try {
       console.warn (opts.method + ' ' + opts.path + ' (' + def.rules.length + ')');
       let [res, data] = await util.httpsRequest (opts, JSON.stringify (body));
       if (res.statusCode != 200)
-        throw new Error ('Error: ' + res.statusCode);
+        console.warn ('Error ' + res.statusCode + ' (' + def.name + ')');
     }, Promise.resolve());
   }, Promise.resolve());
 } catch (e) {
